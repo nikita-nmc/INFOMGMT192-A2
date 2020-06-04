2eeaa96d769ca8c6c2721d9102732d6fe58d2bf5
Nikita Roumiantsev <niknz2000@gmail.com>
2020-06-04 18:24:54 +1200

    Template for wireframe almost complete

---
+++ A2-JS.js
@@ -0,0 +1,11 @@
+function displayBerlin() {
+    $("#berlin").text("- Live in a hostel with 5 other exchange students\n" +
+        "- Organised cultural exchange programmes at local schools and businesses\n" +
+        "- Experience the centre of 20th century European history - a city divided in ideology\n" +
+        "Excursions to countryside breweries for authentic German brewing and beer-tasting experience\n" +
+        "Kickstart your future career with a German-centric focus!\n");
+    }
+
+    $("#myButton").on("click", function() {
+        $("#myName").text("Jose");
+    });
