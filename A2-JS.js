7e12c2c8ddbe1ae7cc8b66d3095be85f75c96bdb
Nikita Roumiantsev <niknz2000@gmail.com>
2020-06-12 14:38:41 +1200

    cleaned up code and re did JS function

--- A2-JS.js
+++ A2-JS.js
@@ -1,8 +1,7 @@
 function toggle(id, ele) {
-    let cont = document.getElementById(id);
-    if (cont.style.visibility == 'visible') {
+    let cont = document.getElementById(id);         //fetch the specific html element to work with
+    if (cont.style.visibility == 'visible') {       //check whether it is already visible or hidden and flip
         cont.style.visibility = 'hidden';
-
         document.getElementById(ele.id).innerText = 'Show More';
     }
     else {
@@ -12,10 +11,18 @@ function toggle(id, ele) {
 }
 
 $(document).ready(function(){
-    $("#myInput").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $("#exchanges li").filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    $("#myInput").on("keyup", function() {          //when the user stops typing in search field, perform function
+        let exchanges = $("#exchanges li");
+        let value = $(this).val().toLowerCase().trim();
+        for (let li of exchanges) {                //loops through the list items of the list of exchanges and hides all
+            let exchange = $(li);
+            exchange.hide();
+        }
+        for (let li of exchanges) {
+            let toView = $(li);
+            if (toView.text().toLowerCase().includes(value)) {  //if an element matches the search query, un-hide it
+                toView.show();
+            }
+        }
         });
     });
-});
